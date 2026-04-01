01118a1042faf15f2bd95762701d7438402a9426
Alumno <alumno@et26.local>
2026-04-01 15:22:00 -0300

    Ejercicios

---
+++ ejer3.js
@@ -0,0 +1,20 @@
+var numero1 = 5
+var numero2 = 8
+
+
+function nums(){
+if(numero1 < numero2) {
+console.log("numero1 no es mayor que numero2");
+}
+if(numero2> 0) {
+console.log("numero2 es positivo");
+}
+if(numero1 <=0) {
+console.log("numero1 es negativo o distinto de cero");
+}
+ else {
+
+
+    console.log("error")
+ }
+}
