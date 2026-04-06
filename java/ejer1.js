104382aeaf68fd0a0047852becfed285c1b6227b
Alumno <alumno@et26.local>
2026-04-06 17:22:09 -0300

    ejerciciosJs

--- java/ejer1.js
+++ java/ejer1.js
@@ -1,9 +1,9 @@
 function meses(){
 
-    const meses = [
+    const losmeses = [
           "Enero", "Febrero", "Marzo", "Abril",
   "Mayo", "Junio", "Julio", "Agosto",
   "Septiembre", "Octubre", "Noviembre", "Diciembre"
     ];
-    console.log(meses);
+    console.log(losmeses);
 }
---
+++ java/ejer2.js
@@ -0,0 +1,33 @@
+function valor(){
+
+var valores = [true, 5, false, "hola", "adios",
+2];
+
+var suma = (valores[1]+valores[5]);
+var resta = (valores[1]-valores[5]);
+var poten = (valores[1]**valores[5]);
+var divi = (valores[1]/valores[5]);
+var multi = (valores[1]*valores[5]);
+
+console.log("La suma es: ", suma);
+console.log("La resta es: ", resta);
+console.log("La potencia es: ", poten);
+console.log("La division es: ", divi);
+console.log("La multiplicacion es: ", multi);
+
+
+
+if(  valores[3].length < valores[4].length) {
+
+console.log("La afirmación es: ", valores[0])
+
+}
+
+if(  valores[3].length > valores[4].length) {
+
+console.log("La afirmación es: ", valores[2])
+
+}
+
+}
+
